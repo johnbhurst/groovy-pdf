7030173d59044f9f6db0eece8a6e8f21d86ec76e
James.L.Williams <James.L.Williams@5612f945-bf37-0410-815c-5f4d545c29e3>
2007-09-09 15:11:46 +0000

    Add iTextVersion example.
    git-svn-id: http://groovy-pdf.googlecode.com/svn/trunk@10 5612f945-bf37-0410-815c-5f4d545c29e3

---
+++ examples/iTextVersion.groovy
@@ -0,0 +1,18 @@
+//iTextVersion.groovy
+//
+// @author jwill
+import be.jameswilliams.PDFBuilder
+import com.lowagie.text.pdf.*
+import com.lowagie.text.PageSize
+import com.lowagie.text.Rectangle
+import com.lowagie.text.Document
+
+try {
+	def builder = new PDFBuilder()
+	def file = 'examples' + File.separator + 'output' + File.separator + 'version.pdf'
+	def a = builder.document(filename:file){
+			paragraph(text:"This page was made using " + Document.getVersion())
+	}
+} catch (Exception e) {
+	e.printStackTrace()
+}
